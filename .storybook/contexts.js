702f5c0aa1de62cf23cdfcd7623d2d8370e435a9
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2020-10-31 22:04:56 -0700

    Storybook themes and add-ons

---
+++ .storybook/contexts.js
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../src/systems";
+
+export const contexts = [
+  {
+    icon: "box",
+    title: "Themes",
+    components: [ThemeProvider],
+    params: [
+      { name: "Light Theme", props: { theme: lightTheme, default: true } },
+      { name: "Dark Theme", props: { theme: darkTheme } }
+    ],
+    options: {
+      deep: true,
+      disable: false,
+      cancelable: false
+    }
+  }
+];
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import { addParameters } from "@storybook/react";
+import { addDecorator } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
+import { withContexts } from "@storybook/addon-contexts/react";
+import { contexts } from "./contexts";
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+addParameters({
+  backgrounds: [
+    { name: "Light theme", value: "#f7fafc", default: true },
+    { name: "Dark theme", value: "#1a202c" }
+  ]
+});
+
+addDecorator(withKnobs);
+addDecorator(withA11y);
+addDecorator(withContexts(contexts));
